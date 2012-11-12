96b2592a4ac7aa608ffd19178bba93fe990247a3
Simon Tennant <simon@buddycloud.com>
2012-11-12 15:57:35 +0100

    Added rough flow
    From email thread in buddycloud-dev mail list.

--- check.py
+++ check.py
@@ -2,6 +2,34 @@
 
 # "Copright 2012, Severin Kistner"
 
+# rough flow:
+#    $domain=”please enter your domain name”
+#    $XMPP-SERVER=”host -t SRV _xmpp-server._tcp.$domain”
+#    $API-SERVER=”host -t SRV _buddycloud-api-server._tcp.$domain”
+#    $WEBCLIENT=”host -t SRV _buddycloud-webclient._tcp.$domain”
+#    # check the buddycoud-server is also in DNS.
+#    $BC_COMPONENT=”buddycloud.$XMPP-SERVER”
+#    $BC_ANON=”anon.$XMPP-SERVER”
+#    $BC_MEDIA=”media.$XMPP-SERVER”
+#    $BC_PUSHER=”pusher.$XMPP-SERVER”
+#    $BC_TOPICS=”topics.$XMPP-SERVER”
+#    try to connect to the $XMPP-SERVER
+#    try to register a user on $XMPP-SERVER
+#    try to remove that user from $XMPP-SERVER
+#    try to connect as an anonymous user on $XMPP-SERVER
+#    DISCO for the buddycloud component on $XMPP-SERVER
+#    try to connect to the buddycloud component on $XMPP-SERVER
+#    try to connect to $API-SERVER server on HTTP
+#    via $API-SERVER, try to retrieve a known open channel from a remote domain
+#    try to connect to the media server on XMPP
+#    try to connect to the media server on HTTP
+#    try to retrieve a known avatar
+#    try to retrieve a known piece of media
+#    xmpp ping the pusher component (don’t know how else to test it)
+#    grep for a known string in config.js from https://$WEBCLIENT/config.js
+
+
+
 import dns.resolver
 import logging
 from optparse import OptionParser
